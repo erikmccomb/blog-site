aff1d5ed31c1c411085e18c5213efff7f684d094
McComb <mccomb@McCombs-MacBook-Pro.local>
2018-01-28 13:19:15 -0700

    Feckin styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer'; 
 
 const TemplateWrapper = ({ children, history }) => (
- <div style={{backgroundColor: '#2b4141', height: '100vh'}}>
+ <div style={{backgroundColor: '#2b4141', height: '100%', minHeight: '100%', paddingBottom: '10vh'}}>
     <Helmet
       title="Dragon on Your Shirt"
       meta={[
